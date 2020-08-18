25aa1118d310dfef683b3fd3712956e683b6295b
Paolo <paolo.suero@gmail.com>
2020-08-18 17:15:03 +0200

    Corretto e validato

--- main.js
+++ main.js
@@ -10,12 +10,12 @@ console.log(surname);
 var favourite_color = prompt("Inserisci il tuo colore preferito");
 console.log(favourite_color);
 
-// generare la password come somma
+// generare la password con la somma delle stringhe inserite dall'utente
 var unsafe_password = name + surname + favourite_color + "20";
 console.log(unsafe_password);
 
 //inserisco la password creata nel html tramite un elemento già presente
 document.getElementById("password").innerHTML = unsafe_password;
 
-//rendo il contenuto della pagina invisibile durante l'inserimento dei dati richiesti nel prompt
+//rendo il contenuto della pagina visibile solo dopo il totale caricamento della pagina
 document.getElementById("password-container").setAttribute("class", "visible");
